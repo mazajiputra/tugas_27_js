626069aea083df20bde2a2a01bc3e9e719771ab4
mazajiputra <mazajiputra@gmail.com>
2021-11-02 04:59:37 +0700

    Kumpul Tugas

---
+++ tugas27.js
@@ -0,0 +1,10 @@
+function tugas() {
+    var array1 = ["No"," rumah saya ",30,"A"];
+    var array2 = ["Jalan pangeran Tirtayasa No",70];
+    var var1 = array1.join("");
+    var var2 = array2.join(" ");
+    console.log(var1);
+    console.log(var2);
+}
+
+tugas()
